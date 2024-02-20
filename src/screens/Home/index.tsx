11cf6ad0972f93b9f8544c86cdb1fa181ceb51e0
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-02-20 20:10:48 -0300

    feat: Input de dados

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,8 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput } from "react-native";
 
 import { styles } from "./style";
 
-export default function Home() {
+export  function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -12,6 +12,12 @@ export default function Home() {
       <Text style={styles.eventDate}>
         Sexta, 4 de Novembro de 2022.
       </Text>
+
+      <TextInput 
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
     </View>
   )
 }
