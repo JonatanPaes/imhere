93392975b5ea4393e6064e626617cc4a0244d665
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-02-21 19:18:57 -0300

    chore: configurando eslint-plugin-simple-import-sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort'],
   extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
   rules: {
+    'simple-import-sort/imports': 'error',
     'prettier/prettier': 'error'
   }
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 
 import { styles } from './style'
 
